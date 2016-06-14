cb43450fe5b46c8ba4e1c2c4d6a60d91c355b233
Rafal Bodek-QPT836 <rafal.bodek@motorolasolutions.com>
2016-06-14 11:27:55 +0200

    init

--- infrastructure/base.js
+++ infrastructure/base.js
@@ -2,23 +2,29 @@ var heroin = require('heroin-js');
 
 var configurator = heroin(process.env.HEROKU_API_TOKEN);
 
-var base = {
-    region: 'eu',
+var base = { name: 'book-inventory-book',
+    organization: undefined,
+    region: 'us',
+    maintenance: false,
     stack: 'cedar-14',
-    config_vars: {MONGODB_URI: process.env.MONGODB_URI},
-    collaborators: ['kwasniewski.mateusz@gmail.com'],
-    features: {
-        'runtime-dyno-metadata': {enabled: false},
-        'log-runtime-metrics': {enabled: false},
-        'http-session-affinity': {enabled: false},
-        preboot: {enabled: false},
-        'http-shard-header': {enabled: false},
-        'http-end-to-end-continue': {enabled: false},
-        'http-sni': {enabled: false}
-    },
-    formation: [{process: 'web', quantity: 1, size: 'Free'}],
-    log_drains: []
-};
+    config_vars:
+    { MONGODB_URI: 'mongodb://heroku_t8wt5hz7:tif7glvkt3747qgq4mpfgk3q46@ds011492.mlab.com:11492/heroku_t8wt5hz7',
+        NODE_ENV: 'production' },
+    addons: {},
+    collaborators:
+        [ 'krzysztof.sniezynski@motorolasolutions.com',
+            'rafal.bodek@gmail.com' ],
+    features:
+    { 'runtime-dyno-metadata': { enabled: false },
+        'log-runtime-metrics': { enabled: false },
+        'http-session-affinity': { enabled: false },
+        preboot: { enabled: false },
+        'http-shard-header': { enabled: false },
+        'http-end-to-end-continue': { enabled: false },
+        'http-sni': { enabled: false } },
+    formation: [ { process: 'web', quantity: 1, size: 'Free' } ],
+    log_drains: [],
+    domains: [ 'book-inventory-book.herokuapp.com' ] };
 
 module.exports = {
     configurator: configurator,
